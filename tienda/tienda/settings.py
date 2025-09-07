4e38b9059a250ebd34f7c12b66c3ac1ea7cc62fa
Diego Cea <diego.cea10@inacapmail.cl>
2025-09-07 20:35:59 -0300

    Archivos y configuración para despliegue en Railway

--- tienda/tienda/settings.py
+++ tienda/tienda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9=cnp0$yh8t+&(*)=0uc#ky&p66-$cg2m0(nq!g+4ufwb@6gkm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
